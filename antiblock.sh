7fb2fd5c46149cd5e399836da85f7b2602ed1d6a
cfgtsp <69192198+cfgtsp5192@users.noreply.github.com>
2023-02-23 21:55:47 +0800

    Update antiblock.sh

--- antiblock.sh
+++ antiblock.sh
@@ -1,12 +1,57 @@
 # AntiJamf program for MacOS (works with Catalina-Ventura on both Intel and Silicon, won't work on linux because its a MacOS MDM)
 # Make sure homebrew is installed and you have installed 'pidof' (if not, run 'brew install pidof')
-# Written by cfgtsp5192, have fun with your unrestricted device
+
+# ----------------------- #
+# 			  #
+#  Written by cfgtsp5192  #
+#			  #
+# ----------------------- #
 
 #!/bin/bash
 
-for (( ; ; )) do 
-    sudo kill -9 $(pidof jamf) > /dev/null
-    sudo kill -9 $(pidof jamfHelper) > /dev/null
-    sudo kill -9 $(pidof JamfDaemon) > /dev/null
-    sudo kill -9 $(pidof JamfProCommService) > /dev/null
+J="jamf"
+JH="jamfHelper"
+JD="JamfDaemon"
+JP="JamfProCommService"
+
+while true
+do
+	Jamf=$(pidof $J)
+	JamfHelper=$(pidof $JH)
+	JamfDaemon=$(pidof $JD)
+	JamfProCommService=$(pidof $JP)
+
+	if [[ $Jamf == "" ]]
+	then
+		sleep 0.1
+		continue
+	else
+		sudo kill -9 $Jamf > /dev/null
+	fi
+
+	if [[ $JamfHelper == "" ]]
+	then
+		sleep 0.1
+		continue
+	else
+		sudo kill -9 $JamfHelper > /dev/null
+	fi
+
+	if [[ $JamfDaemon == "" ]]
+	then
+		sleep 0.1
+		continue
+	else
+		sudo kill -9 $JamfDaemon > /dev/null
+	fi
+
+	if [[ $JamfProCommService == "" ]]
+	then
+		sleep 0.1
+		continue
+	else
+		sudo kill -9 $JamfProCommService > /dev/null
+	fi
+
+	sleep 0.1
 done
